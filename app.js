3999c49e7b35188f75160e3a81a686991efe156c
Rohan Kute <rohanbk309@gmail.com>
2024-01-10 22:57:16 +0530

    Added flash and session config

--- app.js
+++ app.js
@@ -9,20 +9,40 @@ const bodyParser = require('body-parser');
 const ValError = require('./validation/ValError');
 const  {validateAsycFn} = require('./validation/ValidationAsync');
 const  {validateSchema , validateReviewSchema}  = require('./validation/SchemaValidate');
-const engine = require('ejs-mate');
+const ejsMate = require('ejs-mate');
 var methodOverride = require('method-override');
 const campRoute = require('./routes/campRoutes');
 const cudRoute = require('./routes/cudRoutes');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine' , 'ejs');
 app.set('views', path.join(__dirname , 'views'));
-app.engine('ejs', engine);
+app.engine('ejs', ejsMate);
 app.use(express.static('assets')) 
+
+app.use(session({
+  secret: 'secreteforsession',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires : Date.now() * 1000 * 60 * 60 * 24 * 7,
+    httpOnly : true,
+    maxAge : 1000 * 60 * 60 * 24 * 7
+  }
+}));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+})
+
 app.use('/viewcamps' , campRoute)
 app.use('/' , cudRoute);
-
 // connecting to DB 'yelp_camp'
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
@@ -53,6 +73,7 @@ app.use((err , req , res , next)=>{
   if (!err.message) err.message = 'Oh No, Something Went Wrong!'
   res.status(statusCode).render('campgrounds/error', { err })
 })
+
 
 
 
--- routes/cudRoutes.js
+++ routes/cudRoutes.js
@@ -9,26 +9,30 @@ const  {validateSchema , validateReviewSchema}  = require('../validation/SchemaV
 
 
 router.post('/savecamp' ,validateSchema, (async (req , res , next)=>{  
-              const newCamp = new Campground(req.body);
-              await newCamp.save();
-              res.redirect('/viewcamps');
+          const newCamp = new Campground(req.body);
+          await newCamp.save();
+          req.flash('success' , 'Added new camp successfully');
+          res.redirect('/viewcamps');
   
 }));
 
 router.get('/editcamp/:id' , async (req ,res) => {
-  const campground = await Campground.findById(req.params.id);
-  res.render('campgrounds/editcamp' , {campground});
+        const campground = await Campground.findById(req.params.id);
+        req.flash('success' , 'Edited the camp succesfully');
+        res.render('campgrounds/editcamp' , {campground});
 });
 
 router.put('/updatecamp/:id' , validateSchema, validateAsycFn(async (req , res , next) =>{
         await Campground.findByIdAndUpdate(req.params.id,req.body);
+        req.flash('success', 'Successfully updated campground!');
         res.redirect(`/viewcamps/campdetails/${req.params.id}`);
    
 }))
 
 router.delete('/deletecamp/:id' , async (req , res) =>{
-  await Campground.findByIdAndDelete(req.params.id);
-  res.redirect(`/viewcamps`);
+      await Campground.findByIdAndDelete(req.params.id);
+      req.flash('success', 'Successfully deleted campground!');
+      res.redirect(`/viewcamps`);
 });
 
 module.exports = router;
